3dd615d2ddc4f498b4ff6648c323cb434b71ced1
songsunkook <songsunkook@gmail.com>
2025-12-10 17:51:51 +0900

    feat: Consumer AutoConfiguration 추가
    Co-authored-by: cookie-meringue <daehyeon3351@gmail.com>

---
+++ consumer/src/main/java/org/mmmq/consumer/config/ConsumerConfiguration.java
@@ -0,0 +1,9 @@
+package org.mmmq.consumer.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan(basePackages = "org.mmmq.consumer")
+public class ConsumerConfiguration {
+}
