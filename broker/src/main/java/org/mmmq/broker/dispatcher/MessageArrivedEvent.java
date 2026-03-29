834fde9aebfdb99700318948d1c20656d4ac6871
KimDaehyeon <daehyeon3351@gmail.com>
2026-03-30 01:45:46 +0900

    refactor: ApplicationEvent를 상속받지 않는 record 객체로 변경
    Co-authored-by: songsunkook <songsunkook@gmail.com>

--- broker/src/main/java/org/mmmq/broker/dispatcher/MessageArrivedEvent.java
+++ broker/src/main/java/org/mmmq/broker/dispatcher/MessageArrivedEvent.java
@@ -1,17 +1,6 @@
 package org.mmmq.broker.dispatcher;
 
-import org.springframework.context.ApplicationEvent;
-
-class MessageArrivedEvent extends ApplicationEvent {
-
-    private final TopicQueue topicQueue;
-
-    MessageArrivedEvent(TopicQueue topicQueue) {
-        super(topicQueue);
-        this.topicQueue = topicQueue;
-    }
-
-    TopicQueue getTopicQueue() {
-        return topicQueue;
-    }
+record MessageArrivedEvent(
+        TopicQueue topicQueue
+) {
 }
