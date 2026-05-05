248505c53485177488bb622011ffa8e4b6ee405e
KimDaehyeon <daehyeon3351@gmail.com>
2026-05-05 23:21:48 +0900

    refactor: Offset의 미사용 Comparable 구현 제거
    - compareTo와 Comparable<Offset> 인터페이스를 사용하는 곳이 코드베이스에 없어 dead code 제거.
    - Offset은 sequential consume 대상이라 sort 시나리오가 없음.
    
    Co-authored-by: songsunkook <songsunkook@gmail.com>

--- broker/src/main/java/org/mmmq/broker/topicqueue/Offset.java
+++ broker/src/main/java/org/mmmq/broker/topicqueue/Offset.java
@@ -2,14 +2,9 @@ package org.mmmq.broker.topicqueue;
 
 public record Offset(
         long value
-) implements Comparable<Offset> {
+) {
 
     public Offset next() {
         return new Offset(value + 1);
-    }
-
-    @Override
-    public int compareTo(Offset other) {
-        return Long.compare(value, other.value);
     }
 }
